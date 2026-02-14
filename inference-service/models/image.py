ec376cd4c58f6ec36d7a6457a7fa082b2308dba1
Prerana M N <160386927+pmn2305@users.noreply.github.com>
2026-02-14 21:01:22 +0530

    Update image.py

--- inference-service/models/image.py
+++ inference-service/models/image.py
@@ -19,31 +19,33 @@ class ImageModerationModel:
         print(" NEW IMAGE MODEL CODE RUNNING ")
 
         try:
-            image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
+           image = Image.open(io.BytesIO(image_bytes)).convert("RGB")
         except Exception as e:
-            raise ValueError(f"Invalid image file: {str(e)}")
+          raise ValueError(f"Invalid image file: {str(e)}")
 
-        # Preprocess
-        inputs = self.processor(images=image, return_tensors="pt")
+        inputs = self.processor(
+           images=image,
+           return_tensors="pt",
+           padding=True   # <-- ADD THIS
+        )
 
         with torch.no_grad():
-            outputs = self.model(**inputs)
+           outputs = self.model(**inputs)
 
         logits = outputs.logits
         probs = torch.softmax(logits, dim=1)[0]
 
-        # Get labels
         id2label = self.model.config.id2label
-
+  
         nsfw_score = 0.0
 
         for idx, prob in enumerate(probs):
-            label = id2label[idx].lower()
-            if label in self.NSFW_LABELS:
-                nsfw_score = float(prob.item())
-                break
+           label = id2label[idx].lower()
+           if label in self.NSFW_LABELS:
+            nsfw_score = float(prob.item())
+            break
 
         return {
-            "score": nsfw_score,
-            "model_version": self.model_version
-        }
+           "score": nsfw_score,
+           "model_version": self.model_version
+       }
